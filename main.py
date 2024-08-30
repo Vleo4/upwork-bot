ee72ab57f2b398296cedaa1ca3988b48efc1d7a3
Vleo4 <volodya.leopard@gmail.com>
2024-08-30 05:43:56 +0300

    init

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from job_feed import find_jobs
 
 options = webdriver.ChromeOptions()
 #options.add_experimental_option("detach", True)
+options.add_argument("--headless=new")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-extensions")
 browser = webdriver.Chrome(options=options)
 
 find_jobs(browser=browser)
